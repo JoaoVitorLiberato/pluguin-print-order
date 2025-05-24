6c6810a91d3a5aa472d9458426cc12fa48b5a36c
João Vitor L. de Oliveira <j.liberato@vertex.vertexdigital.co>
2025-05-24 15:46:26 -0300

    Adicionando v-for para pegar ips, porque com sync e await pega apenas o ultimo

--- src/Infra/Pluguins/Printer.pluguin.ts
+++ src/Infra/Pluguins/Printer.pluguin.ts
@@ -1,22 +1,14 @@
 import { ThermalPrinter, PrinterTypes, BreakLine, CharacterSet } from "node-thermal-printer"
-import { printerIP } from "../../../ip.config"
 
-export default (async () => {
-  const IPs = await printerIP()
-  let configPrinter
-
-  for (const ip of IPs) {
-    configPrinter = new ThermalPrinter({
-      type: PrinterTypes.EPSON,
-      interface: String(ip),
-      removeSpecialCharacters: true,
-      breakLine: BreakLine.WORD,
-      characterSet: CharacterSet.PC437_USA,
-      options: {
-        timeout: 30000,
-      }
-    })
-  }
-
-  return configPrinter as ThermalPrinter
-})()
+export const configPrinter = (ip:string) => {
+  return new ThermalPrinter({
+    type: PrinterTypes.EPSON,
+    interface: String(ip),
+    removeSpecialCharacters: true,
+    breakLine: BreakLine.WORD,
+    characterSet: CharacterSet.PC437_USA,
+    options: {
+      timeout: 30000,
+    }
+  })
+}
